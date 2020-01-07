f36a0e5e1f4ab685da2fb381edf4df96bb5ab804
Mitchel Vroege <19216221+MitchelAnthony@users.noreply.github.com>
2020-01-07 22:13:56 +0100

    Added some mock data, made the API (except PUT) dynamic using the mock data and updated the tests

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 use std::collections::HashMap;
+use std::sync::RwLock;
 
 #[macro_use] extern crate rocket;
 #[macro_use] extern crate rocket_contrib;
@@ -10,6 +11,7 @@ use std::collections::HashMap;
 
 use rocket::http::Status;
 use rocket::response::status;
+use rocket::State;
 use rocket_contrib::json::{Json, JsonValue};
 use rocket_contrib::templates::Template;
 
@@ -21,14 +23,20 @@ fn index() -> Template {
 }
 
 // EVENTS API
-#[derive(Serialize, Deserialize, Debug)]
+type EventsList = RwLock<HashMap<usize, Event>>;
+
+#[derive(Serialize, Deserialize, Clone)]
 struct Event {
     id: Option<usize>,
     title: String,
 }
 
 #[get("/?<page>")]
-fn get_events(page: Option<usize>) -> JsonValue {
+fn get_events(page: Option<usize>, events_list: State<EventsList>) -> JsonValue {
+    let events_list = events_list.read().unwrap();
+    let id = next_insert_id(events_list.keys().collect::<Vec<&usize>>());
+
+    // TODO Implement paging when switching to database (hashmap is unordered and iterates in no particular order) 
     let page = if let Some(i) = page {
         i
     } else {
@@ -36,41 +44,65 @@ fn get_events(page: Option<usize>) -> JsonValue {
     };
 
     json!({
-        "events": [],
+        "events": *events_list,
         "page": page,
     })
 }
 
 #[get("/<id>")]
-fn get_event(id: usize) -> Json<Event> {
-    Json(Event {
-        id: Some(10),
-        title: String::from("First Event"),
-    })
+fn get_event(id: usize, events_list: State<EventsList>) -> Option<Json<Event>> {
+    let events_list = events_list.read().unwrap();
+
+    if events_list.contains_key(&id) {
+        Some(Json(
+            events_list.get(&id).unwrap().clone()
+        ))
+    } else {
+        None
+    }
 }
 
 #[delete("/<id>")]
-fn delete_event(id: usize) -> status::Custom<JsonValue> {
-    status::Custom(
-        Status::NoContent,
-        json!({
-            "status": "ok",
-            "id": id,
-        })
-    )
+fn delete_event(id: usize, events_list: State<EventsList>) -> Option<status::Custom<JsonValue>> {
+    let events_list = &mut events_list.write().unwrap();
+
+    if events_list.contains_key(&id) {
+        events_list.remove_entry(&id);
+
+        Some(status::Custom(
+            Status::NoContent,
+            json!({
+                "status": "ok",
+                "id": id,
+            })
+        ))
+    } else {
+        None
+    }
 }
 
 #[post("/", data = "<body>")]
-fn new_event(body: Json<Event>) -> status::Created<Json<Event>> {
+fn new_event(body: Json<Event>, events_list: State<EventsList>) -> status::Created<Json<Event>> {
+    let events_list = &mut events_list.write().unwrap();
+
+    let id = next_insert_id(events_list.keys().collect::<Vec<&usize>>());
+    let mut event = body.into_inner();
+    event.id = Some(id);
+
+    events_list.insert(id, event.clone());
+
     status::Created(
-        uri!("/events", get_event: 10).to_string(),
+        uri!("/events", get_event: id).to_string(),
         Some(Json(
-            Event {
-                id: Some(10),
-                title: String::from("First Event"),
-            }
+            event
         ))
     )
+}
+
+fn next_insert_id(mut current_ids: Vec<&usize>) -> usize {
+    current_ids.sort();
+
+    current_ids.pop().unwrap() + 1
 }
 
 #[put("/<id>", data = "<body>")]
@@ -82,10 +114,19 @@ fn update_event(id: usize, body: Json<Event>) -> Json<Event> {
 }
 
 fn rocket() -> rocket::Rocket {
+    // Create mock database
+    let mut events_hashmap = HashMap::new();
+    events_hashmap.insert(1, Event { id: Some(1), title: String::from("First Event")});
+    events_hashmap.insert(2, Event { id: Some(2), title: String::from("Second Event")});
+    events_hashmap.insert(3, Event { id: Some(3), title: String::from("Third Event")});
+    
+    let events_list: EventsList = RwLock::new(events_hashmap);
+
     rocket::ignite()
         .mount("/", routes![index])
         .mount("/events", routes![get_events, get_event, delete_event, new_event, update_event])
         .attach(Template::fairing())
+        .manage(events_list)
 }
 
 fn main() {
--- src/tests.rs
+++ src/tests.rs
@@ -5,36 +5,105 @@ use rocket::local::Client;
 #[test]
 fn test_get_events() {
     let client = Client::new(rocket()).unwrap();
-    let response = client.get("/events").dispatch();
 
+    // Test default response
+    let mut response = client.get("/events").dispatch();
+    let mut expected_body = r#"{"events":{"1":{"id":1,"title":"First Event"},"2":{"id":2,"title":"Second Event"},"3":{"id":3,"title":"Third Event"}},"page":1}"#.to_string();
+    
     assert_eq!(response.status(), Status::Ok);
+    assert_eq!(response.body_string(), Some(expected_body));
+
+    // Test response for existing page
+    // TODO This test currently only checks if the page parameter is correctly parsed, update expected_body when paging is implemented
+    response = client.get("/events?page=2").dispatch();
+    expected_body = r#"{"events":{"1":{"id":1,"title":"First Event"},"2":{"id":2,"title":"Second Event"},"3":{"id":3,"title":"Third Event"}},"page":2}"#.to_string();
+    
+    assert_eq!(response.status(), Status::Ok);
+    assert_eq!(response.body_string(), Some(expected_body));
+
+    // Test response for non existing page
+    // response = client.get("/events?page=3").dispatch();
+    // expected_body = r#"{"events":{"1":{"id":1,"title":"First Event"},"2":{"id":2,"title":"Second Event"},"3":{"id":3,"title":"Third Event"}},"page":3}"#.to_string();
+    
+    // assert_eq!(response.status(), Status::Ok);
+    // assert_eq!(response.body_string(), Some(expected_body));
+
+    // Test response for invalid page value
+    response = client.get("/events?page=test").dispatch();
+    expected_body = r#"{"events":{"1":{"id":1,"title":"First Event"},"2":{"id":2,"title":"Second Event"},"3":{"id":3,"title":"Third Event"}},"page":1}"#.to_string();
+    
+    assert_eq!(response.status(), Status::Ok);
+    assert_eq!(response.body_string(), Some(expected_body));
 }
 
 #[test]
 fn test_get_event() {
     let client = Client::new(rocket()).unwrap();
-    let response = client.get("/events/10").dispatch();
+
+    // Test existing event response
+    let mut response = client.get("/events/1").dispatch();
+    let expected_body = r#"{"id":1,"title":"First Event"}"#.to_string();
 
     assert_eq!(response.status(), Status::Ok);
+    assert_eq!(response.body_string(), Some(expected_body));
+
+    // Test non existing event response
+    response = client.get("/events/10").dispatch();
+
+    assert_eq!(response.status(), Status::NotFound);
+
+    // Test invalid event response
+    response = client.get("/events/test").dispatch();
+
+    assert_eq!(response.status(), Status::NotFound);
 }
 
 #[test]
 fn test_delete_event() {
     let client = Client::new(rocket()).unwrap();
-    let response = client.delete("/events/10").dispatch();
+    
+    // Test existing event delete
+    let mut response = client.delete("/events/1").dispatch();
 
     assert_eq!(response.status(), Status::NoContent);
+
+    // Test if the event is actually removed
+    response = client.get("/events/1").dispatch();
+
+    assert_eq!(response.status(), Status::NotFound);
+
+    // Test non existing event delete
+    response = client.get("/events/10").dispatch();
+
+    assert_eq!(response.status(), Status::NotFound);
+
+    // Test invalid event delete
+    response = client.get("/events/test").dispatch();
+
+    assert_eq!(response.status(), Status::NotFound);
 }
 
 #[test]
 fn test_new_event() {
     let client = Client::new(rocket()).unwrap();
+
+    // Test new event
     let response = client.post("/events")
-        .body(r#"{"id":10,"title":"Test Event"}"#)
+        .body(r#"{"title":"Fourth Event"}"#)
         .dispatch()
     ;
+    let expected_body = "/events/4";
 
     assert_eq!(response.status(), Status::Created);
+    assert_eq!(response.headers().get_one("Location"), Some(expected_body));
+
+    // Test if the event is actually created
+    let location = response.headers().get_one("Location").unwrap();
+    let mut response = client.get(location).dispatch();
+    let expected_body = r#"{"id":4,"title":"Fourth Event"}"#.to_string();
+
+    assert_eq!(response.status(), Status::Ok);
+    assert_eq!(response.body_string(), Some(expected_body));
 }
 
 #[test]
